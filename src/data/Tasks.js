70f746dd91d231cb24b2ce19357951e434258365
alejandrodavidromero <romero.alejandrodavid@gmail.com>
2022-10-08 17:20:07 -0300

    first commit

---
+++ src/data/Tasks.js
@@ -0,0 +1,19 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Romero Alejandro - Henry 1 ",
+    description: "Homework del M2, react",
+  },
+
+  {
+    id: 1,
+    title: "Cata Romero - Técnica 5to C",
+    description: "Proyecto del usuario/login",
+  },
+
+  {
+    id: 2,
+    title: "Paula Tabaso - PF",
+    description: "Licencia médica",
+  },
+];
